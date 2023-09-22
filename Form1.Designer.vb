f26ed641d6f9d0659cbf42339fdc02ed2110fb24
magero97 <hillary.magero@student.utamu.ac.ug>
2023-09-22 00:51:07 -0400

    commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.btnInvoice = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(151, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(151, 114)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "ID"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(151, 163)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(108, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Car Type"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(151, 212)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(118, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Start Date"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(151, 272)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(109, 25)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "End Date"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(151, 322)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(66, 25)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Price"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(379, 61)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(244, 31)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(379, 319)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 31)
+        Me.TextBox3.TabIndex = 9
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(379, 108)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(244, 31)
+        Me.TextBox2.TabIndex = 11
+        '
+        'btnInvoice
+        '
+        Me.btnInvoice.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInvoice.Location = New System.Drawing.Point(156, 397)
+        Me.btnInvoice.Name = "btnInvoice"
+        Me.btnInvoice.Size = New System.Drawing.Size(213, 44)
+        Me.btnInvoice.TabIndex = 12
+        Me.btnInvoice.Text = "Generate Invoice"
+        Me.btnInvoice.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(379, 155)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(244, 33)
+        Me.ComboBox1.TabIndex = 13
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker1.Location = New System.Drawing.Point(379, 212)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(244, 31)
+        Me.DateTimePicker1.TabIndex = 14
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.CustomFormat = ""
+        Me.DateTimePicker2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker2.Location = New System.Drawing.Point(379, 272)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(244, 31)
+        Me.DateTimePicker2.TabIndex = 15
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(490, 325)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(133, 25)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Ugx Per Day"
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPrint.Location = New System.Drawing.Point(488, 397)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(91, 44)
+        Me.btnPrint.TabIndex = 17
+        Me.btnPrint.Text = "Print"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.Red
+        Me.Label8.Location = New System.Drawing.Point(179, 108)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(21, 25)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "*"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.ForeColor = System.Drawing.Color.Red
+        Me.Label9.Location = New System.Drawing.Point(242, 9)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(327, 25)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "THE AGABAS CAR CONNECT"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BackgroundImage = Global.Agaba_Car_Connect.My.Resources.Resources.impreza
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.btnInvoice)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Agaba Car Connect"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents btnInvoice As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents DateTimePicker2 As DateTimePicker
+    Friend WithEvents Label7 As Label
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,127 @@
+﻿Public Class Form1
+
+    Private invoiceNumber As Integer = 1 ' Initialize invoice number to 1
+
+    ' Initialize your form by loading car types into the ComboBox
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.Add("Subaru")
+        ComboBox1.Items.Add("Fuso")
+        ComboBox1.Items.Add("Noah")
+        ComboBox1.Items.Add("Corolla")
+        ComboBox1.SelectedIndex = 0 ' Default selected item
+        UpdatePriceTextbox()
+    End Sub
+
+    ' Function to get car rental rate
+    Private Function GetRentalRate(carType As String) As Integer
+        Select Case carType
+            Case "Subaru"
+                Return 100000
+            Case "Fuso"
+                Return 150000
+            Case "Noah"
+                Return 75000
+            Case "Corolla"
+                Return 50000
+            Case Else
+                Return 0
+        End Select
+    End Function
+
+    Private Sub UpdatePriceTextbox()
+        Dim rate As Integer = GetRentalRate(ComboBox1.SelectedItem.ToString())
+        TextBox3.Text = rate.ToString("N0")
+    End Sub
+
+    ' Update the Price textbox when a new car type is selected
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        UpdatePriceTextbox()
+    End Sub
+
+    ' When the "Rent" button is clicked
+    Private Sub btnInvoice_Click(sender As Object, e As EventArgs) Handles btnInvoice.Click
+        ' Validate ID
+        If String.IsNullOrWhiteSpace(TextBox2.Text) Then
+            MessageBox.Show("Please enter a valid ID.")
+            Return
+        End If
+
+        Dim daysOfRental As Integer
+
+        Dim startDate As Date = DateTimePicker1.Value
+        Dim endDate As Date = DateTimePicker2.Value
+
+        daysOfRental = (endDate - startDate).Days + 1
+
+        ' Ensure end date is not before start date
+        If daysOfRental <= 0 Then
+            MessageBox.Show("End date can't be earlier than start date.")
+            Return
+        End If
+
+        Dim rate As Integer = GetRentalRate(ComboBox1.SelectedItem.ToString())
+        Dim totalPrice As Integer = daysOfRental * rate
+
+        Dim currentInvoiceNumber As Integer = invoiceNumber
+        invoiceNumber += 1
+
+        ' Display invoice with the unique invoice number
+        Dim invoice As String = $"Invoice #{currentInvoiceNumber}:{vbNewLine}" &
+                                $"Name: {TextBox1.Text}{vbNewLine}" &
+                                $"ID: {TextBox2.Text}{vbNewLine}" &
+                                $"Car Type: {ComboBox1.SelectedItem}{vbNewLine}" &
+                                $"Start Date: {startDate.ToShortDateString()}{vbNewLine}" &
+                                $"End Date: {endDate.ToShortDateString()}{vbNewLine}" &
+                                $"Total Amount: {totalPrice.ToString("N0")} UGX"
+
+        ' Show the invoice
+        MessageBox.Show(invoice)
+
+        ' After displaying the invoice, prompt the user to print it
+        Dim printDialog As New PrintDialog()
+        If printDialog.ShowDialog() = DialogResult.OK Then
+            Dim printDocument As New Printing.PrintDocument()
+            AddHandler printDocument.PrintPage, AddressOf PrintDocument_PrintPage
+            printDocument.Print()
+        End If
+    End Sub
+
+    ' Handler for printing the invoice
+    Private Sub PrintDocument_PrintPage(sender As Object, e As Printing.PrintPageEventArgs)
+        Dim invoice As String = GetInvoiceText()
+        Dim font As New Font("Arial", 12)
+        Dim brush As New SolidBrush(Color.Black)
+        Dim pageBounds As RectangleF = e.MarginBounds
+        Dim textSize As SizeF = e.Graphics.MeasureString(invoice, font, pageBounds.Width)
+        Dim x As Single = (pageBounds.Width - textSize.Width) / 2
+        Dim y As Single = pageBounds.Top
+        e.Graphics.DrawString(invoice, font, brush, New PointF(x, y))
+    End Sub
+
+    ' Function to get the formatted invoice text
+    Private Function GetInvoiceText() As String
+        Dim startDate As Date = DateTimePicker1.Value
+        Dim endDate As Date = DateTimePicker2.Value
+        Dim daysOfRental As Integer = (endDate - startDate).Days + 1
+        Dim rate As Integer = GetRentalRate(ComboBox1.SelectedItem.ToString())
+        Dim totalPrice As Integer = daysOfRental * rate
+        Dim invoice As String = $"Invoice #{invoiceNumber}:{vbNewLine}" &
+                                $"Name: {TextBox1.Text}{vbNewLine}" &
+                                $"ID: {TextBox2.Text}{vbNewLine}" &
+                                $"Car Type: {ComboBox1.SelectedItem}{vbNewLine}" &
+                                $"Start Date: {startDate.ToShortDateString()}{vbNewLine}" &
+                                $"End Date: {endDate.ToShortDateString()}{vbNewLine}" &
+                                $"Total Amount: {totalPrice.ToString("N0")} UGX"
+        Return invoice
+    End Function
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        ' Prompt the user to print the invoice
+        Dim printDialog As New PrintDialog()
+        If printDialog.ShowDialog() = DialogResult.OK Then
+            Dim printDocument As New Printing.PrintDocument()
+            AddHandler printDocument.PrintPage, AddressOf PrintDocument_PrintPage
+            printDocument.Print()
+        End If
+    End Sub
+End Class
